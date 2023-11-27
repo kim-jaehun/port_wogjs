0f89757e999c7398b3a36c785c8184b5e039ca00
[github <[github>
2023-11-28 00:19:50 +0900

    헤더셋완

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -11,6 +11,6 @@ if (headerToggle) {
         : headerToggle.setAttribute("aria-expanded", "true");//속성값변경
     });
     //헤더토글이 존재하는 경우에만 이후 코드블록 실행(오류방지)
-    //addEventListener=모바일 토글버튼 클릭시 동작
+    //addEventListener=모바일 토글버튼 클릭시 동작2
     //headerNav.classList.toggle("show") = show를 토글링 /있으면제거 없으면 추가
 }
