e6b2a75e00337c39c2f3f7a2712af11750195042
[github <[github>
2023-12-04 10:56:09 +0900

    마무리

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,7 +1,17 @@
-export function link(){
-    console.log(link) 
-}
+export function link() {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
 
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+}
 
 // 콘솔로그로 실행 
 //import와 export를 사용하는 점
